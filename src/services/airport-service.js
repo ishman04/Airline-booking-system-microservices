d937e3e4a07f5f6beb7075012682017c3d88ab43
Ishman Jeet Singh <ishmanalagh@gmail.com>
2025-04-05 18:28:57 +0530

    Created airport crud

---
+++ src/services/airport-service.js
@@ -0,0 +1,60 @@
+const { StatusCodes } = require('http-status-codes');
+const {AirportRepository} = require('../repositories');
+const AppError = require('../utils/errors/app-error');
+
+const airportRepository = new AirportRepository();
+
+async function createAirport(data){
+    try {
+        const airport = await airportRepository.create(data);
+        return airport;
+    } catch (error) {
+        if(error.name == 'SequelizeValidationError'){
+            let explanation = []
+            error.errors.forEach((err) => {
+                explanation.push(err.message);
+            })
+            throw new AppError(explanation,StatusCodes.BAD_REQUEST)
+        }
+        throw new AppError('Cannot create a new airport object',StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+async function getAirports(){
+    try {
+        const airports = await airportRepository.getAll();
+        return airports;
+    } catch (error) {
+        throw new AppError('Cannot fetch data of all airports',StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+}
+    async function getAirport(id){
+        try {
+            const airport = await airportRepository.get(id);
+            return airport;
+        } catch (error) {
+            if(error.statusCode == StatusCodes.NOT_FOUND){
+                throw new AppError('Requested airport not present', StatusCodes.NOT_FOUND)
+            }
+            throw new AppError('Cannot fetch data of airport',StatusCodes.INTERNAL_SERVER_ERROR)
+        }
+}
+
+async function destroyAirport(id){
+    try {
+        const airport = await airportRepository.destroy(id);
+        return airport
+    } catch (error) {
+        if(error.statusCode == StatusCodes.NOT_FOUND){
+            throw new AppError('Requested airport not present', StatusCodes.NOT_FOUND)
+        }
+        throw new AppError('Cannot fetch data of all airports',StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+}
+
+module.exports = {
+    createAirport,
+    getAirport,
+    getAirport,
+    destroyAirport
+}
